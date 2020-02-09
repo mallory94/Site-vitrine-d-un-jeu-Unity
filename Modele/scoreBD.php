b98b9fe0c8f800af4ccbbe77967083f94b8793e7
mallory94 <malloga@hotmail.fr>
2020-02-09 16:59:44 +0100

    ecriture des fonctions et requetes recuperant les scores

--- Modele/scoreBD.php
+++ Modele/scoreBD.php
@@ -12,7 +12,6 @@ function getListeCategorieBD(){
 
 	try{
 		$commande = $pdo->prepare($sql);
-        $commande->bindParam(':idTest', $id_test);
 		$bool = $commande->execute();
 		
 		if($bool){
@@ -38,21 +37,19 @@ function getListeCategorieBD(){
 function getListeScoreBD(){
 	require ("../modele/connect.php"); 
 
-    $sql = "SELECT column_name 
-    FROM information_schema.columns 
-    WHERE table_name='score' 
-    AND table_schema='base-pjs4'";
+    $sql = "SELECT *
+			FROM score as s
+			ORDER BY s.meilleurScore DESC";
     
     $resultat= array();
 
 	try{
 		$commande = $pdo->prepare($sql);
-        $commande->bindParam(':idTest', $id_test);
 		$bool = $commande->execute();
 		
 		if($bool){
 			$resultat = $commande->fetchAll(PDO::FETCH_ASSOC);
-			//var_dump($resultat);
+			var_dump($resultat);
 			return $resultat;
 		}
 		else{
