983c0ffb0fcbd37d3d2a44ecce48a4d859bf84fd
mallory94 <malloga@hotmail.fr>
2020-04-01 05:43:33 +0200

    mise en commentaire du changement de dossier courant afin de se conformer au docker de gitlab

--- tests/ConnexionTest.php
+++ tests/ConnexionTest.php
@@ -9,31 +9,31 @@ class ConnexionTest extends TestCase  {
     
 
     public function test_mdpDifferents(){
-        chdir("../../");
-        echo ("répertoire courant = " . getcwd());
+        // chdir("../../");
+        // echo ("répertoire courant = " . getcwd());
         require_once("./controleur/utilisateur.php");
         $this->assertFalse(mdpDifferents("oh","oh"));
     }
     
     public function test_pseudoInexistant(){
-        chdir("../../");
+        // chdir("../../");
         $this->assertFalse(pseudoInexistant("Coop3"));
     }
 
     public function test_emailDejaUtilise(){
-        chdir("../../");
+        // chdir("../../");
         require_once("./controleur/utilisateur.php");
         $this->assertTrue(emailDejaUtilise("gare@gmail.com"));
     }
 
     public function test_pseudoDejaPris(){
-        chdir("../../");
+        // chdir("../../");
         require_once("./controleur/utilisateur.php");
         $this->assertTrue(pseudoDejaPris("Coop3"));
     }
 
     public function test_recupMdpBD(){
-        chdir("../../");
+        // chdir("../../");
         require_once("./controleur/utilisateur.php");
         $this->assertEquals(recupMdpBD("Coop3"), "OJ8UHN3D0");
     }
