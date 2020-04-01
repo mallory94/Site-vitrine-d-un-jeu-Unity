0e404280013af2f93e84c6b9dc4f0da9819f65c4
mallory94 <malloga@hotmail.fr>
2020-04-01 05:35:03 +0200

    ajout de tests Unitaires

--- tests/ConnexionTest.php
+++ tests/ConnexionTest.php
@@ -4,10 +4,49 @@ use PHPUnit\Framework\TestCase;
 
 class ConnexionTest extends TestCase  {
 
-    public function testMdpDifferents(){
-        require("../../controleur/utilisateur.php");
+    
+
+    
+
+    public function test_mdpDifferents(){
+        chdir("../../");
+        echo ("répertoire courant = " . getcwd());
+        require_once("./controleur/utilisateur.php");
         $this->assertFalse(mdpDifferents("oh","oh"));
     }
+    
+    public function test_pseudoInexistant(){
+        chdir("../../");
+        $this->assertFalse(pseudoInexistant("Coop3"));
+    }
+
+    public function test_emailDejaUtilise(){
+        chdir("../../");
+        require_once("./controleur/utilisateur.php");
+        $this->assertTrue(emailDejaUtilise("gare@gmail.com"));
+    }
+
+    public function test_pseudoDejaPris(){
+        chdir("../../");
+        require_once("./controleur/utilisateur.php");
+        $this->assertTrue(pseudoDejaPris("Coop3"));
+    }
+
+    public function test_recupMdpBD(){
+        chdir("../../");
+        require_once("./controleur/utilisateur.php");
+        $this->assertEquals(recupMdpBD("Coop3"), "OJ8UHN3D0");
+    }
+
+    // public function test_(){
+    //     chdir("../../");
+    //     require_once("./controleur/utilisateur.php");
+    //     $this->assert
+    // }
+    
+    
+
+
 }
 
 
