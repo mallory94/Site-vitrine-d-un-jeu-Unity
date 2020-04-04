c6d6840186ed18a49a1876998a9698cec2a70b37
mallory94 <malloga@hotmail.fr>
2020-04-04 19:56:13 +0200

    modifications de l'affichage des tests PHPUNIT

--- tests/ConnexionTest.php
+++ tests/ConnexionTest.php
@@ -10,42 +10,40 @@ class ConnexionTest extends TestCase  {
 
     public function test_mdpDifferents(){
         // chdir("../../");
-        echo ("répertoire courant = " . getcwd());
+        echo("-------------------------------------\n\n");
+        echo ("répertoire courant = " . getcwd() . "\n\n");
         require_once("./controleur/utilisateur.php");
-        echo("lancement de la fonction test_mdpDifferents\n");
+        echo("lancement du test de la fonction mdpDifferents\n");
         $this->assertFalse(mdpDifferents("oh","oh"));
     }
     
     public function test_pseudoInexistant(){
         // chdir("../../");
+        echo("lancement du test de la fonction pseudoInexistant\n");
         $this->assertFalse(pseudoInexistant("Coop3"));
     }
 
     public function test_emailDejaUtilise(){
         // chdir("../../");
         require_once("./controleur/utilisateur.php");
+        echo("lancement du test de la fonction emailDejaUtilise\n");
         $this->assertTrue(emailDejaUtilise("gare@gmail.com"));
     }
 
     public function test_pseudoDejaPris(){
         // chdir("../../");
         require_once("./controleur/utilisateur.php");
+        echo("lancement du test de la fonction pseudoDejaPris\n");
         $this->assertTrue(pseudoDejaPris("Coop3"));
     }
 
     public function test_recupMdpBD(){
         // chdir("../../");
-        require_once("./controleur/utilisateur.php");
+        //require_once("./controleur/utilisateur.php");
+        echo("lancement du test de la fonction recupMdpBD\n");
         $this->assertEquals(recupMdpBD("Coop3"), "OJ8UHN3D0");
+        echo("-------------------------------------\n\n");
     }
-
-    // public function test_(){
-    //     chdir("../../");
-    //     require_once("./controleur/utilisateur.php");
-    //     $this->assert
-    // }
-    
-    
 
 
 }
